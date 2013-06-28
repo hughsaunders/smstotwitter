1ca189b6c0f227e313f05db09cfc8e9228781d91
Hugh Saunders <hugh@wherenow.org>
2013-06-28 10:32:04 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='smstotwitter',
+    verison='0.1',
+    packages=['smstotwitter'],
+    author='Hugh Saunders',
+    author_email='hugh@wherenow.org'
+)
+
